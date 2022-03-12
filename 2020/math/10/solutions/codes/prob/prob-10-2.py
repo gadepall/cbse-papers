33a2df1b8d64e6efa5c78512e2f0a0e1311d70c1
gadepall <gadepall@gmail.com>
2022-03-12 09:08:09 +0530

    Add existing files

--- 2020/math/10/solutions/codes/prob/prob-10-2.py
+++ 2020/math/10/solutions/codes/prob/prob-10-2.py
@@ -28,86 +28,51 @@ import shlex
 
 
 #Input parameters
-base= np.linspace(10,70,7)
-values = np.array([5,15,20,25,15,11,9])
+base= np.linspace(0,70,8)
+base = base.reshape([1,8])
+values = np.array(([5,15,20,25,15,11,9]))
+#base= np.array([104,119,127,135,144,152,162, 170,182])
+#values = np.array([305,552,540,472,377,304,250,200])
+nvalues = np.size(values)
 
-#obtain histogram values 
-#values, base = np.histogram(data, bins)
+print(base,values)
+A = np.block([base],[values])
 
 #find the cumulative sums
 cumulative = np.cumsum(values)
+cumulative = np.block([0,cumulative])
+imed = np.searchsorted(cumulative, cumulative[nvalues]/2, side='right', sorter=None)
+med = base[imed-1] + (cumulative[nvalues]/2-cumulative[imed-1])/values[imed-1]*(base[imed]-base[imed-1])
 
-#print(bins, data)
-print(median(values), base, cumulative[6]/2)
+#print(med, imed,base[imed-1], cumulative[nvalues]/2,cumulative[imed-1], values[imed-1])#,med,cumulative[imed-2])
+
+medcoord = np.array([med,cumulative[nvalues]/2])
 
 # plot the ogive
 plt.plot(base, cumulative, 'ro-')
-#plt.plot(base[:-1], cumulative, 'ro-')
-plt.grid()
+#plt.xlim(100,200)
+plt.xlabel('Class Interval')
+plt.ylabel('Cumulative Frequency')
+plt.legend(loc='best')
+plt.grid() # minor
+#plt.axis('equal')
+
+#Labeling the coordinates
+tri_coords = medcoord.T
+plt.scatter(tri_coords[0], tri_coords[1])
+vert_labels = ['median']
+for i, txt in enumerate(vert_labels):
+    plt.annotate(txt, # this is the text
+                 (tri_coords[0], tri_coords[1]), # this is the point to label
+                 textcoords="offset points", # how to position the text
+                 xytext=(0,10), # distance from text to points (x,y)
+                 ha='center') # horizontal alignment can be left, right or center
+
 
 #if using termux
 plt.savefig('/storage/emulated/0/github/cbse-papers/2020/math/10/solutions/figs/prob-10-2.pdf')
 subprocess.run(shlex.split("termux-open /storage/emulated/0/github/cbse-papers/2020/math/10/solutions/figs/prob-10-2.pdf"))
 
-
-
-#I =  np.eye(2)
-#e1 =  I[:,0]
-##Input parameters
-#
-##Circle parameters
-#r = 5
-#B = np.zeros(2)
-#O =  np.array(([0,r])) #normal vector
-#P =  np.array(([-3,0])) #normal vector
-#thetadeg = 100
-#theta = thetadeg*np.pi/180
-#ab = 2*r*np.sin(theta/2)
-#A =  ab*np.array(([-np.cos(theta/2),np.sin(theta/2)]))
-#
-#
-###Generating the line 
-#m = e1
-#k1 = -10
-#k2 = 10
-#xline = line_dir_pt(m,B,k1,k2)
-#xAB = line_gen(A,B)
-#xOB = line_gen(O,B)
-#xOA = line_gen(A,O)
-#
-###Generating the circle
-#x_circ= circ_gen(O,r)
-#
-##Plotting all lines
-#plt.plot(xline[0,:],xline[1,:],label='Tangent')
-#plt.plot(xAB[0,:],xAB[1,:],label='Chord')
-#plt.plot(xOA[0,:],xOA[1,:],label='Radius')
-#plt.plot(xOB[0,:],xOB[1,:],label='Radius')
-#
-##Plotting the circle
-#plt.plot(x_circ[0,:],x_circ[1,:],label='Circle')
-#
-#
-##Labeling the coordinates
-#tri_coords = np.vstack((O,A,B,P)).T
-#plt.scatter(tri_coords[0,:], tri_coords[1,:])
-#vert_labels = ['O','A','B','P']
-#for i, txt in enumerate(vert_labels):
-#    plt.annotate(txt, # this is the text
-#                 (tri_coords[0,i], tri_coords[1,i]), # this is the point to label
-#                 textcoords="offset points", # how to position the text
-#                 xytext=(0,10), # distance from text to points (x,y)
-#                 ha='center') # horizontal alignment can be left, right or center
-#
-#plt.xlabel('$x$')
-#plt.ylabel('$y$')
-#plt.legend(loc='best')
-#plt.grid() # minor
-#plt.axis('equal')
-#
-##if using termux
-#plt.savefig('/storage/emulated/0/github/cbse-papers/2020/math/10/solutions/figs/matrix-10-20.pdf')
-#subprocess.run(shlex.split("termux-open /storage/emulated/0/github/school/ncert-vectors/defs/figs/cbse-10-20.pdf"))
 ##else
 ##plt.show()
 #
