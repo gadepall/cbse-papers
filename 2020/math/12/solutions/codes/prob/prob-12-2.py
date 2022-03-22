59b689434bdb450e81d15217c17a29d242164709
gadepall <gadepall@gmail.com>
2022-03-22 05:54:17 +0530

    Add existing files

--- 2020/math/12/solutions/codes/prob/prob-12-2.py
+++ 2020/math/12/solutions/codes/prob/prob-12-2.py
@@ -32,8 +32,8 @@ import shlex
 
 
 #Sample size
-#N = 1e5
-N =100 
+N = int(1e6)
+#N =100 
 
 #Generating Samples
 x = 1+RN.randint(6,size=(2,N))
@@ -50,7 +50,48 @@ second= result[:,n_zeros:N-1]
 
 #Finding the common elements for 0 and 1 (two sixes)
 both = set(first[1]).intersection(second[1])
-print(first, second, both, np.shape(both))
+#print(first, second, both, len(both))
+
+#Printing number of sixes for first dice
+#print(len(first[1]))
+
+#Printing number of sixes for second dice
+#print(len(second[1]))
+
+#Printing number of solo sixes 
+#print(len(first[1])+len(second[1]))
+
+#Probability of single 6
+one6 = (len(first[1])+len(second[1])-2*len(both))/N
+
+#Printing  number of double sixes 
+#print(len(both))
+
+#Probability of double 6
+two6 = len(both)/N
+
+#Probability of no 6
+no6 = 1 - two6 -one6
+
+#Printing the desired probabilities
+print(no6,one6,two6)
+
+#Printing theoretical probabilities
+p = 1/6
+n = 2
+print(mp.binomial(n,0)*(1-p)**2,mp.binomial(n,1)*p*(1-p),mp.binomial(n,2)*p**2*(1-p))
+
+
+#Mean number of 6s
+theory_mean = n*p
+
+#Numerical mean
+num_mean = (len(first[1])+len(second[1])-len(both))/N
+
+print(theory_mean,num_mean)
+
+
+
 
 
 #xy= list(zip(result[0], result[1]))
